e0a8f9fcf1bb0fb0e37b646935599f96218e24ea
osw282 <25309418+osw282@users.noreply.github.com>
2025-04-22 10:49:10 +0100

    Make a oom error when someone tries to buy a t shirt

--- src/checkoutservice/main.go
+++ src/checkoutservice/main.go
@@ -239,12 +239,12 @@ func (cs *checkoutService) PlaceOrder(ctx context.Context, req *pb.PlaceOrderReq
 		return nil, status.Errorf(codes.Internal, err.Error())
 	}
 
-	// Do not allow people to checkout if they got "Salt & Pepper Shakers"
+	// Do not allow people to checkout if they got "T shirt"
 	const memoryThreshold = 2 * 1024 * 1024 * 1024 // 2 GB
 
 	for _, item := range prep.cartItems {
-		if item.GetProductId() == "LS4PSXUNUM" { // Product ID for "Salt & Pepper Shakers"
-			log.Println("Allocating 2 GB of memory due to 'Salt & Pepper Shakers' in cart.")
+		if item.GetProductId() == "AAAAAAAAA2" { // Product ID for "T shirt"
+			log.Println("Allocating 2 GB of memory due to 'T shirt ' in cart.")
 			memoryEater := make([]byte, memoryThreshold)
 			// Use the memory to prevent optimization
 			for i := range memoryEater {
